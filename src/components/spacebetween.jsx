9702ad8f44fbc394c1d984020728063e13def113
cokbill <cokordabilly@gmail.com>
2023-06-07 14:28:01 +0800

    koding akademi

---
+++ src/components/spacebetween.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function SpaceBetween() {
+    return (
+        <>
+        <div className="w-[800px] mx-auto flex">
+            <div className="img">
+                
+            </div>
+            <div className="button">
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default SpaceBetween;
