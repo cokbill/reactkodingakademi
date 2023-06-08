134f9d8b8ffb62163d1a17e5e673ea24b5a84dad
cokbill <cokordabilly@gmail.com>
2023-06-08 15:58:55 +0800

    lanjut

--- src/App.js
+++ src/App.js
@@ -3,12 +3,57 @@ import Salam from "./selamatdatang";
 import Copyright from "./copyright";
 
 function App() {
-  return (
-    
+  // create js variable 
+  const cityName = "Denpasar";
+  let number1 = 200;
+  let number2 = 800;
+
+  console.log(cityName);
+  console.log(number1);
+  console.log(number2);
+
+  // sum 
+  const sum = (number1, number2) => {
+    return number1 + number2
+  }
+
+console.log(sum(number1, number2));
+
+// looping fn 
+const sample = (name) => {
+  let word = "";
+ for (let i = 0; i < 3; i++) {
+   word = word + name + " ";
+ };
+   return word
+};
+console.log(sample('dodi'));
+
+
+// 
+  return (  
     <div className="w-full h-screen text-center bg-slate-700">
-      <SpaceBetween />
+     <div className="text-3xl text-white">
+     2.a {cityName}
+      </div>  
+     <div className="text-3xl text-white">
+     2.b {number1}
+      </div>  
+     <div className="text-3xl text-white">
+     2.c {number2}
+      </div>    
+     <div className="text-3xl text-white">
+     {sum(number1, number2)}
+      </div>    
+     <div className="text-3xl text-white">
+      4.a {sample('dodi')}
+      </div>    
+     <div className="text-3xl text-white">
+      4.b {sample('150')}
+      </div>    
+      {/* <SpaceBetween />
       <Salam/>
-      <Copyright/>
+      <Copyright/> */}
     </div>
   );
 }
